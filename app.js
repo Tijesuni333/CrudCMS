fa9b948fcf75cee1cb099e2277e6efdd3501c557
Tijesuni333 <99971409+Tijesuni333@users.noreply.github.com>
2023-12-16 21:05:30 +0100

    final commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let form = document.getElementById("form");
 let input = document.getElementById("input");
+let input2 = document.getElementById("input2");
+let input3 = document.getElementById("input3");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
 
@@ -11,33 +13,40 @@ form.addEventListener("submit", (e) => {
 let data ={};
 let acceptData =()=>{
     data["text"]= input.value;
+    data["email"]= input2.value;
+    data["number"]= input3.value;
     console.log(data);
 
     createPost();
 };
 let createPost =()=>{
     posts.innerHTML += 
-    `   <table>
-    <tr>
+    
+    `<tr>
         <td>${data.text}</td>
-        <td>${data.text}/td>
-        <td>${data.text}</td>
+        <td>${data.email}</td>
+        <td>${data.number}</td>
         <td>
             <span class="options">
                 <i onClick="editPost(this)" class="fas fa-edit"></i>
                 <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
               </span>
         </td>
-    </tr>
-</table>`;
+    </tr>`
     input.value = "";
+    input2.value = "";
+    input3.value= "";
 }
 
 let deletePost = (e)=>{
-    e.parentElement.parentElement.remove();
+    e.parentElement.parentElement.parentElement.remove();
 };
 
 let editPost = (e) => {
-    input.value = e.parentElement.previousElementSibling.innerHTML;
-    e.parentElement.parentElement.remove();
+    input.value = e.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
+    input2.value = e.parentElement.parentElement.previousElementSibling.previousElementSibling.innerHTML;
+    input3.value = e.parentElement.parentElement.previousElementSibling.innerHTML;
+   
+   
+    e.parentElement.parentElement.parentElement.remove();
 };
